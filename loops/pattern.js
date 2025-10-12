b37bf6e68201d19301fdd806f7ac2c583b72454d
mohitvashu1 <mohitchoubeybca@gmail.com>
2025-10-12 21:03:12 +0530

    pattern problems

---
+++ loops/pattern.js
@@ -0,0 +1,37 @@
+// Pattern 1
+let n = 4 ;
+for ( let row = 1; row <= n; row++ ) {
+    let pattern = '';
+    for ( let col = 1; col <= row; col++ ) {
+        pattern += '* ';
+    }
+    console.log( pattern );
+}
+console.log("         ")
+// Pattern 2
+let num = 4 ;
+for ( let row = 1; row <= num; row++ ) {
+    let pattern = '';
+    for ( let col = 1; col <=num; col++ ) {
+        pattern += '* ';
+    }
+    console.log( pattern );
+}
+
+console.log("         ")
+// Pattern 3
+
+let number = 4;
+
+for ( let row = 1; row <= number; row++ ) {
+    let pattern = ' ';
+    let spaces = number - row;
+    for ( let s = 1; s <= spaces; s++ ) {
+        pattern += ' ';
+    }
+    for ( let col = 1; col <= row; col++ ) {
+        pattern += '*';
+    }
+    console.log( pattern );
+
+}
