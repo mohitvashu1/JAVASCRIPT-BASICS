1d60af861beec4cf7af45d4e0679c19a7db9574e
mohitvashu1 <mohitchoubeybca@gmail.com>
2025-10-12 21:02:52 +0530

    coercion

---
+++ coercion/implicitCoercion.js
@@ -0,0 +1,18 @@
+ // implicitCoercion
+let a = 2 + '2';
+console.log(a);
+
+let b = '2' * 2;
+console.log(b);
+
+let c = '2' - '2';
+console.log(c);
+
+let d = '2' / '2';
+console.log(d);
+
+let e = '2' % '2';
+console.log(e);
+
+let f = '2' > '12';
+console.log(f);
