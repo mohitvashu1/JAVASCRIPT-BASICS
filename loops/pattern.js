3a0362e4f4c3d07fb26152d805482a6af74aed1b
mohitvashu1 <mohitchoubeybca@gmail.com>
2025-10-31 22:34:17 +0530

    pattern question

--- loops/pattern.js
+++ loops/pattern.js
@@ -1,40 +1,40 @@
-// Pattern 1
-let n = 4 ;
-for ( let row = 1; row <= n; row++ ) {
-    let pattern = '';
-    for ( let col = 1; col <= row; col++ ) {
-        pattern += '* ';
-    }
-    console.log( pattern );
-}
-console.log("         ")
-// Pattern 2
-let num = 4 ;
-for ( let row = 1; row <= num; row++ ) {
-    let pattern = '';
-    for ( let col = 1; col <=num; col++ ) {
-        pattern += '* ';
-    }
-    console.log( pattern );
-}
+// // Pattern 1
+// let n = 4 ;
+// for ( let row = 1; row <= n; row++ ) {
+//     let pattern = '';
+//     for ( let col = 1; col <= row; col++ ) {
+//         pattern += '* ';
+//     }
+//     console.log( pattern );
+// }
+// console.log("         ")
+// // Pattern 2
+// let num = 4 ;
+// for ( let row = 1; row <= num; row++ ) {
+//     let pattern = '';
+//     for ( let col = 1; col <=num; col++ ) {
+//         pattern += '* ';
+//     }
+//     console.log( pattern );
+// }
 
-console.log("         ")
-// Pattern 3
+// console.log("         ")
+// // Pattern 3
 
-let number = 4;
+// let number = 4;
 
-for ( let row = 1; row <= number; row++ ) {
-    let pattern = ' ';
-    let spaces = number - row;
-    for ( let s = 1; s <= spaces; s++ ) {
-        pattern += ' ';
-    }
-    for ( let col = 1; col <= row; col++ ) {
-        pattern += '*';
-    }
-    console.log( pattern );
+// for ( let row = 1; row <= number; row++ ) {
+//     let pattern = ' ';
+//     let spaces = number - row;
+//     for ( let s = 1; s <= spaces; s++ ) {
+//         pattern += ' ';
+//     }
+//     for ( let col = 1; col <= row; col++ ) {
+//         pattern += '*';
+//     }
+//     console.log( pattern );
 
-}
+// }
 
-console.log("         ")
-// Pattern 4
+// console.log("         ")
+// // Pattern 4
